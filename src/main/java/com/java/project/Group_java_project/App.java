2d5f483c0fc3da3ecdff73e60e3dc38d70a78433
14-Prasanna <2k22cse115@kiot.ac.in>
2026-03-10 15:48:06 +0530

    Maven file folder with quickstart 1.1v

---
+++ src/main/java/com/java/project/Group_java_project/App.java
@@ -0,0 +1,7 @@
+package com.java.project.Group_java_project;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ src/test/java/com/java/project/Group_java_project/AppTest.java
@@ -0,0 +1,13 @@
+package com.java.project.Group_java_project;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
