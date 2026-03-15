c85930a0e6285e1aefe3f0b98749e7b20c1e144f
14-Prasanna <2k22cse115@kiot.ac.in>
2026-03-15 11:43:04 +0530

    Customer_login page class created

---
+++ src/main/java/com/java/project/Group_java_project/Restaurant_details/Customer_Login.java
@@ -0,0 +1,10 @@
+package com.java.project.Group_java_project.Restaurant_details;
+
+public class Customer_Login {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
